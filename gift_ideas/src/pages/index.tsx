0321d8c3fdae36f04c24419ad99696cbeb0e8dae
Deepan <deepan.c01@gmail.com>
2022-11-26 18:02:58 +0530

    ...

--- gift_ideas/src/pages/index.tsx
+++ gift_ideas/src/pages/index.tsx
@@ -2,17 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import Layout from "../../components/Layout";
-import data from '../utils/data'
+//import data from '../utils/data'
 import db from '../utils/db'
+import Product from '../models/Product'
 import ProductItem from '../../components/ProductItem'
-const Home: NextPage = () => {
+const Home: NextPage = ({products}) => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
     <>
       <Layout title={"Home page"}> 
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {data.products.map((product)=>(
+          {products.map((product)=>(
              <ProductItem product={product} key={product.slug}></ProductItem> 
           ))}
         </div>
@@ -25,8 +26,13 @@ const Home: NextPage = () => {
    
 };
 
-const ssp = async function getServerSideProps(){
+export async function getServerSideProps(){
   await db.connect;
-  const product=await Product
+  const products=await Product.find().lean()
+  return{
+    props:{
+      products,
+    },
+  };
 }
-export default [Home,ssp];
+export default Home;
