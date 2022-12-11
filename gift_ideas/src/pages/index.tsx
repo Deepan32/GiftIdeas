7c944a634305654888658e7d0626b840947d67a7
Deepan <deepan.c01@gmail.com>
2022-12-11 13:01:35 +0530

    mongo db changes attempt - not working

--- gift_ideas/src/pages/index.tsx
+++ gift_ideas/src/pages/index.tsx
@@ -3,15 +3,17 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import Layout from "../../components/Layout";
 import data from '../utils/data'
+import db from '../utils/db'
 import ProductItem from '../../components/ProductItem'
-const Home: NextPage = () => {
+import Product from "../models/Product";
+const Home: NextPage = ({products}) => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
     <>
       <Layout title={"Home page"}> 
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {data.products.map((product)=>(
+          {products.map((product)=>(
              <ProductItem product={product} key={product.slug}></ProductItem> 
           ))}
         </div>
@@ -21,5 +23,17 @@ const Home: NextPage = () => {
     </>
   )
 };
+
+export async function getServerSideProps(){
+  await db.connect()
+  const products= await Product.find().lean();
+  console.log('hi products')
+  console.log(products,'products')
+  return (
+    {props:
+      {products:products.map(db.convertDocToObj)}
+    }
+  )
+}
 
 export default Home;
